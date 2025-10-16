be42bbeaecfa6a25ceeeaaa1aa60e5b1d5539865
srikanthpragada <srikanthpragada@gmail.com>
2025-10-16 17:57:40 +0530

    16-OCT

---
+++ reactdemo/src/basics/HelloCC.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+// Class Component 
+export default class HelloCC extends Component {
+  render() {
+    return (
+      <h2>Hello from Class Component </h2>
+    )
+  }
+}
