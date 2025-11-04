9e529587685b2d9dcc97be52a0bd8cec47da02db
srikanthpragada <srikanthpragada@gmail.com>
2025-11-04 17:54:21 +0530

    04-NOV

--- reactdemo/src/index.js
+++ reactdemo/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import DigitalClock from './state/DigitalClockCC';
+import Main from './routing/Main';
 
 
 let products = [ 
@@ -13,7 +13,7 @@ let products = [
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <DigitalClock />
+     <Main />
   </React.StrictMode>
 );
 
